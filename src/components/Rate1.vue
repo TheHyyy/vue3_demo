1c614d641e1d3f9a456b5f55c1552871f2520b19
TheHyyy <447542807@qq.com>
2022-09-04 10:23:37 +0800

    feat: init

---
+++ src/components/Rate1.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>{{ rate }}222</div>
+</template>
+
+<script setup>
+import { defineProps, computed } from "vue";
+let props = defineProps({
+  value: Number,
+});
+let rate = computed(() => "★★★★★☆☆☆☆☆".slice(5 - props.value, 10 - props.value));
+</script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -4,11 +4,18 @@
     <TodoListVue></TodoListVue>
 
     <h1 @click="toggle">click</h1>
+    <Rate :value="score"></Rate>
   </div>
 </template>
 
 <script setup>
 import TodoListVue from "../components/TodoList.vue";
+import Rate from "../components/Rate1.vue";
+
 import { useFullscreen } from "@vueuse/core";
+import { ref } from "vue";
+
+let score = ref(4);
+
 const { isFullscreen, enter, exit, toggle } = useFullscreen();
 </script>
